2c19366b025a30e52bf3cdab1920ecdc708c906e
Lee <rhkd4129@naver.com>
2023-05-19 01:57:46 +0900

    '?'

--- restframework/instagram/urls.py
+++ restframework/instagram/urls.py
@@ -1,7 +1,16 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from . import views
 
-app_name = 'instagram'
-urlpatterns = []
+
+
+router = DefaultRouter()
+router.register("post", views.PostViewSet) #2개의ㅏ url만들어준다 
+# url patterns List
+
+urlpatterns = [
+    path('',include(router.urls)),
+]
 
